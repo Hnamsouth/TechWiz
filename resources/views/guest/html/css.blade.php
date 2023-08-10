52edf8d93ff27fe67dc80ba7484d9d2bdb1620df
Long Nguyễn Hoàng <hlong2k1@gmail.com>
2023-08-10 13:15:39 +0700

    keep some old css stuffs and get some new things

--- resources/views/guest/html/css.blade.php
+++ resources/views/guest/html/css.blade.php
@@ -1,4 +1,24 @@
-<link rel="icon" href="/assets/images/favicon.png">
+<link rel="icon" href="assets/images/favicon.png">
 
 <!-- style -->
-<link rel="stylesheet" href="/assets/css/style.css">
+<link rel="stylesheet" href="assets/css/style.css">
+{{--<link rel="apple-touch-icon" href="assets/apple-touch-icon.png">--}}
+{{--<link rel="stylesheet" href="assets/css/bootstrap.min.css">--}}
+{{--<link rel="stylesheet" href="assets/css/normalize.css">--}}
+{{--<link rel="stylesheet" href="assets/css/font-awesome.min.css">--}}
+{{--<link rel="stylesheet" href="assets/css/transitions.css">--}}
+{{--<link rel="stylesheet" href="assets/css/prettyPhoto.css">--}}
+{{--<link rel="stylesheet" href="assets/css/swiper.min.css">--}}
+{{--<link rel="stylesheet" href="assets/css/jquery-ui.css">--}}
+{{--<link rel="stylesheet" href="assets/css/animate.css">--}}
+{{--<link rel="stylesheet" href="assets/css/owl.theme.css">--}}
+{{--<link rel="stylesheet" href="assets/css/owl.carousel.css">--}}
+{{--<link rel="stylesheet" href="assets/css/owl.theme.default.css">--}}
+{{--<link rel="stylesheet" href="assets/css/customScrollbar.css">--}}
+{{--<link rel="stylesheet" href="assets/css/icomoon.css">--}}
+{{--<link rel="stylesheet" href="assets/css/main.css">--}}
+{{--<link rel="stylesheet" href="assets/css/color.css">--}}
+{{--<link rel="stylesheet" href="assets/css/responsive.css">--}}
+{{--<link rel="stylesheet" href="assets/css/checkout.css">--}}
+{{--<link rel="stylesheet" href="assets/css/header.css">--}}
+{{--<link rel="stylesheet" href="assets/css/user.css">--}}
