0491093f25e9b5b3d4690221155fc712e6ef177f
Nam Hoang <hnam17397@gmail.com>
2023-08-12 17:35:08 +0700

    fix header

--- resources/views/guest/layout.blade.php
+++ resources/views/guest/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <base href="/">
     <!-- ==== Document Title ==== -->
-    <title>sccc</title>
+    <title>Soccer Club</title>
 
     <!-- ==== Document Meta ==== -->
     <meta name="author" content="">
