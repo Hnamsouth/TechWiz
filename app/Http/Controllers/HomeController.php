571b3811fe1af910ffad28078697ebf96abc53aa
Long Nguyễn Hoàng <hlong2k1@gmail.com>
2023-08-12 20:42:33 +0700

    home login fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+//        $this->middleware('auth');
     }
 
     /**
